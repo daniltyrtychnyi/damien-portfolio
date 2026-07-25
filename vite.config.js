4ee51a1ec3cf9af0ac920a8cf1f7419400dc1aee
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-07-25 21:59:17 +0300

    chore: set up scss architecture, aliases and preprocessor config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
 import { defineConfig, pluginSsg } from "minista"
+import path from 'path'
 
 export default defineConfig({
   plugins: [pluginSsg()],
+  resolve: {
+    alias: [{
+      find: '@/',
+      replacement: path.resolve('src') + '/',
+    }],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '@/styles/helpers' as *;
+        `
+      }
+    }
+  }
 })
