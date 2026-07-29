97ce8258e4fce226d95080e67ec302bee3413fbf
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-07-29 11:40:37 +0300

    feat(button): add icon and mode support

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,6 @@
 import './Button.scss'
 import clsx from 'clsx'
+import Icon from '../Icon'
 
 const Button = (props) => {
     const {
@@ -7,29 +8,48 @@ const Button = (props) => {
         type = 'button',
         href,
         target,
+        /**
+         * '' (default) | 'circle'
+         */
+        mode = '',
         label,
         isLabelVisible = false,
+        iconName,
+        /**
+         * 'before' | 'after'
+         */
+        iconPosition = 'before',
     } = props
 
     const isLink = href !== undefined
     const Component = isLink ? 'a' : 'button'
-    const linkProps = { href, target }
-    const buttonProps = { type }
+    const linkProps = {href, target}
+    const buttonProps = {type}
     const specificProps = isLink ? linkProps : buttonProps
     const title = isLabelVisible ? label : undefined
+    const iconComponent = iconName && (
+        <Icon
+            className="button__icon"
+            name={iconName}
+        />
+    )
 
     return (
         <Component
-            className={clsx(className, 'button')}
+            className={clsx(className, 'button', {
+                [`button--${mode}`]: mode,
+            })}
             {...specificProps}
             title={title}
             aria-label={title}
         >
+            {iconPosition === 'before' && iconComponent}
             {!isLabelVisible && (
                 <span className="button__label">
                     {label}
                 </span>
             )}
+            {iconPosition === 'after' && iconComponent}
         </Component>
     )
 }
