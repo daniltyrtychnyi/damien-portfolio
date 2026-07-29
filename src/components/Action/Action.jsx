745a2e94b34ae5fe174efc47af3a88bf30991374
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-07-29 13:50:05 +0300

    feat: add Action component

---
+++ src/components/Action/Action.jsx
@@ -0,0 +1,44 @@
+import './Action.scss'
+import getPropsFromPolymorphicTag from "@/utils/getPropsFromPolymorphicTag"
+import clsx from 'clsx'
+import Icon from '../Icon'
+
+const Action = (props) => {
+    const {
+        className,
+        href,
+        type,
+        title,
+        subTitle,
+    } = props
+
+    const { Component, specificProps } = getPropsFromPolymorphicTag(href, type)
+
+    return (
+        <Component
+            className={clsx(className, 'action')}
+            {...specificProps}
+        >
+            <span className="action__top">
+                <span className="action__title">
+                    {title}
+                </span>
+                <span className="action__icon-wrapper">
+                    <Icon
+                        className="action__icon"
+                        name="arrow-top-right"
+                    />
+                </span>
+            </span>
+            {subTitle && (
+                <span className="action__bottom">
+                    <span className="action__title">
+                        {subTitle}
+                    </span>
+                </span>
+            )}
+        </Component>
+    )
+}
+
+export default Action
