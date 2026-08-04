a3f0fc2f4456328a661363e2444385a7ee20b7dd
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-08-04 22:03:05 +0300

    refactor(about): improve layout section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -26,52 +26,56 @@ const About = () => {
                     loading="lazy"
                 />
                 <div className="about__body">
-                    <div className="about__card">
-                        <div className="about__card-introduction">
-                            <h3 className="about__card-title h4">Introduction</h3>
-                            <div className="about__card-description">
-                                <p>My journey as a photographer has been a lifelong quest to capture the extraordinary
-                                    in the ordinary, to freeze fleeting moments in time, and to share the world's beauty
-                                    as I see it. Based in the enchanting landscapes of the USA, I find inspiration in
-                                    every corner of this diverse and vibrant country. Join me as we embark on a visual
-                                    odyssey, where each photograph tells a story, and every frame is a piece of my
-                                    heart.</p>
-                            </div>
+                    <div className="about__introduction">
+                        <h3 className="about__title h4">
+                            Introduction
+                        </h3>
+                        <div className="about__description">
+                            <p>
+                                My journey as a photographer has been a lifelong quest to capture the extraordinary in
+                                the ordinary, to freeze fleeting moments in time, and to share the world's beauty as I
+                                see it. Based in the enchanting landscapes of the USA, I find inspiration in every
+                                corner of this diverse and vibrant country. Join me as we embark on a visual odyssey,
+                                where each photograph tells a story, and every frame is a piece of my heart.
+                            </p>
                         </div>
                     </div>
-                    <div className="about__card">
-                        <div className="about__card-contact-information">
-                            <h3 className="about__card-title h4">Contact Information</h3>
-                            <div className="about__card-contacts">
-                                <div className="about__card-contact">
-                                    <span className="about__card-label">
-                                        Email
-                                    </span>
-                                    <a href="mailto:damienbraun@gmail.com"
-                                       className="about__card-link">damienbraun@gmail.com</a>
-                                </div>
-                                <div className="about__card-contact">
-                                    <span className="about__card-label">
-                                        Phone Number
-                                    </span>
-                                    <a href="tel:+00000000000" className="about__card-link">+00 000000000</a>
-                                </div>
-                            </div>
-                            <div className="about__card-extra">
-                                <Socials className="about__card-soc1als"/>
-                                <div className="about__card-actions">
-                                    <Button
-                                        className="about__card-button"
-                                        href="/"
-                                        label="Let’s Work"
-                                    />
-                                    <Button
-                                        className="about__card-button"
-                                        href="/src/assets/icons/star.svg"
-                                        label="Download CV"
-                                        download
-                                    />
-                                </div>
+                    <div className="about__contacts">
+                        <h3 className="about__title h4">
+                            Contact Information
+                        </h3>
+                        <ul className="about__contacts-list">
+                            <li className="about__contacts-item">
+                                <span className="about__contacts-label">
+                                    Email
+                                </span>
+                                <a href="mailto:damienbraun@gmail.com" className="about__contacts-link">
+                                    damienbraun@gmail.com
+                                </a>
+                            </li>
+                            <li className="about__contacts-item">
+                                <span className="about__contacts-label">
+                                    Phone Number
+                                </span>
+                                <a href="tel:+00000000000" className="about__contacts-link">
+                                    +00 000000000
+                                </a>
+                            </li>
+                        </ul>
+                        <div className="about__extra">
+                            <Socials className="about__soc1als"/>
+                            <div className="about__actions">
+                                <Button
+                                    className="about__button"
+                                    href="/"
+                                    label="Let’s Work"
+                                />
+                                <Button
+                                    className="about__button"
+                                    href="/src/assets/icons/star.svg"
+                                    label="Download CV"
+                                    download
+                                />
                             </div>
                         </div>
                     </div>
