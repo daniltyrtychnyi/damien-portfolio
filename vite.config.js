b82b4bc7a010222c8ab1762c5cb1e5b0864a8e63
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-08-05 17:42:57 +0300

    chore: configure Vite dev server host

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,30 @@
-import { defineConfig, pluginSsg, pluginBundle, pluginSprite, pluginImage } from "minista"
+import {defineConfig, pluginSsg, pluginBundle, pluginSprite, pluginImage} from "minista"
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-      pluginSsg(),
-      pluginBundle(),
-      pluginSprite(),
-      pluginImage(),
-  ],
-  resolve: {
-    alias: [{
-      find: '@/',
-      replacement: path.resolve('src') + '/',
-    }],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    server: {
+        host: '127.0.0.1',
+        port: 3000,
+    },
+    plugins: [
+        pluginSsg(),
+        pluginBundle(),
+        pluginSprite(),
+        pluginImage(),
+    ],
+    resolve: {
+        alias: [{
+            find: '@/',
+            replacement: path.resolve('src') + '/',
+        }],
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
           @use '@/styles/helpers' as *;
         `
-      }
+            }
+        }
     }
-  }
 })
