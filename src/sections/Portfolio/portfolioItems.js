3c4c5f535bc6151c3ab0cd8416dfa83c402504dc
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-08-06 21:49:58 +0300

    refactor: move static data outside components

---
+++ src/sections/Portfolio/portfolioItems.js
@@ -0,0 +1,58 @@
+const portfolioItems = [
+    {
+        imgSrc: '/src/assets/images/portfolio/1.jpg',
+        title: 'Faces of Resilience',
+        date: 'March 2022',
+        dateTime: '2022-03',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/2.jpg',
+        title: 'A Wedding Tale',
+        date: 'January 2020',
+        dateTime: '2022-01',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/3.jpg',
+        title: 'Product Elegance',
+        date: 'January 2020',
+        dateTime: '2022-01',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/1.jpg',
+        title: 'Faces of Resilience',
+        date: 'March 2022',
+        dateTime: '2022-03',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/2.jpg',
+        title: 'A Wedding Tale',
+        date: 'January 2020',
+        dateTime: '2022-01',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/3.jpg',
+        title: 'Product Elegance',
+        date: 'January 2020',
+        dateTime: '2022-01',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/1.jpg',
+        title: 'Faces of Resilience',
+        date: 'March 2022',
+        dateTime: '2022-03',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/2.jpg',
+        title: 'A Wedding Tale',
+        date: 'January 2020',
+        dateTime: '2022-01',
+    },
+    {
+        imgSrc: '/src/assets/images/portfolio/3.jpg',
+        title: 'Product Elegance',
+        date: 'January 2020',
+        dateTime: '2022-01',
+    },
+]
+
+export default portfolioItems
---
+++ src/sections/Services/servicesItems.js
@@ -0,0 +1,37 @@
+const serviceItems = [
+    {
+        title: 'Events',
+        description: 'Our event photography service is dedicated to capturing the magic of your special occasions. Whether it\'s a wedding, corporate event, or milestone celebration, we\'re there to document every heartfelt moment. We blend into the background, ensuring natural and candid shots that reflect the emotions of the day.',
+        imgSrc: '/src/assets/images/services/1.jpg',
+        features: [
+            'Coverage for weddings, parties, corporate functions, and more.',
+            'Skilled photographers who know how to seize the moment.',
+            'A mix of candid and posed shots for a comprehensive story.',
+            'Quick turnaround for you to relive the day\'s highlights.',
+        ],
+    },
+    {
+        title: 'Events',
+        description: 'Our event photography service is dedicated to capturing the magic of your special occasions. Whether it\'s a wedding, corporate event, or milestone celebration, we\'re there to document every heartfelt moment. We blend into the background, ensuring natural and candid shots that reflect the emotions of the day.',
+        imgSrc: '/src/assets/images/services/1.jpg',
+        features: [
+            'Coverage for weddings, parties, corporate functions, and more.',
+            'Skilled photographers who know how to seize the moment.',
+            'A mix of candid and posed shots for a comprehensive story.',
+            'Quick turnaround for you to relive the day\'s highlights.',
+        ],
+    },
+    {
+        title: 'Events',
+        description: 'Our event photography service is dedicated to capturing the magic of your special occasions. Whether it\'s a wedding, corporate event, or milestone celebration, we\'re there to document every heartfelt moment. We blend into the background, ensuring natural and candid shots that reflect the emotions of the day.',
+        imgSrc: '/src/assets/images/services/1.jpg',
+        features: [
+            'Coverage for weddings, parties, corporate functions, and more.',
+            'Skilled photographers who know how to seize the moment.',
+            'A mix of candid and posed shots for a comprehensive story.',
+            'Quick turnaround for you to relive the day\'s highlights.',
+        ],
+    },
+]
+
+export default serviceItems
