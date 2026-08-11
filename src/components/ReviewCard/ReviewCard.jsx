648f46f6cd7e7e17a99ac9b09f0775d3ccd193c9
Danil Tyrtychnyi <daniltyrtychnyi@yandex.ru>
2026-08-11 20:41:41 +0300

    feat: add ReviewCard component

---
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -0,0 +1,58 @@
+import './ReviewCard.scss'
+import Socials from '@/components/Socials'
+
+export default (props) => {
+    const {
+        name,
+        location,
+        description,
+        value,
+    } = props
+
+    return (
+        <article className="review-card">
+            <header className="review-card__header">
+                <div className="review-card__author">
+                    <h3 className="review-card__name h6">
+                        {name}
+                    </h3>
+                    <span className="review-card__location">
+                        {location}
+                    </span>
+                </div>
+                <Socials
+                    className="review-card__soc1als"
+                />
+            </header>
+            <div
+                className="review-card__stars"
+                aria-label={`Rating ${value} out of 5`}
+                style={{
+                    '--ratingStarsValue': value,
+                }}
+            >
+                <img
+                    src="/rating/stars_unfilled.svg"
+                    alt=""
+                    className="review-card__stars-unfilled"
+                    width={140}
+                    height={24}
+                    loading="lazy"
+                />
+                <img
+                    src="/rating/stars_filled.svg"
+                    alt=""
+                    className="review-card__stars-filled"
+                    width={140}
+                    height={24}
+                    loading="lazy"
+                />
+            </div>
+            <div className="review-card__description">
+                <p>
+                    {description}
+                </p>
+            </div>
+        </article>
+    )
+}
